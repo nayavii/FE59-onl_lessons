a254fc3d62e3a5e5d27749c82cfac3798b2d8dde
Karasevgen1205 <karasevgen1205@yandex.ru>
2024-04-21 23:34:51 +0300

    add some examples to hw_review

--- hw_review.js
+++ hw_review.js
@@ -2,6 +2,41 @@
 
 // стилизация элементов через классы по БЭМу (блок, элемент, модификатор)
 
+// banner_bottom__block__container-input
+{
+  /* <section class="banner">
+  <div class="banner__top">
+    <div class="banner__top__module">
+        <button class="banner__top__module-button">Delete All</button>
+        <button class="banner__top__module-button margin-button">Delete Last</button>
+        <input class="banner__top__module-input" type="text" id="name" placeholder="Enter todo ...">
+        <button class="banner__top__module-button">Add</button>
+    </div>
+    <div class="banner__bottom__module">
+        <p class="banner__bottom__module-cikle">All:2</p>
+        <p class="banner__bottom__module-cikle">Complited:1</p>
+        <button class="banner__bottom__module-button">Show All</button>
+        <button class="banner__bottom__module-button margin-button">Show Completed</button>
+        <input class="banner__bottom__module-input" type="text" id="name" placeholder="Search ...">
+    </div>
+  </div>   
+  <div class="banner__bottom">
+      <div class="banner__bottom__block__container">
+          <input class="banner_bottom__block__container-input" type="checkbox">
+          <p class="banner__bottom__block__container-text">Todo text</p>
+          <button class="banner__bottom__block__container-button">x</button>
+          <p class="banner__bottom__block__container-textDate">Date</p>
+      </div>
+      <div class="banner__bottom__block__container">
+          <input class="banner_bottom__block__container-input" type="checkbox">
+          <p class="banner__bottom__block__container-text">Todo text</p>
+          <button class="banner__bottom__block__container-button">x</button>
+          <p class="banner__bottom__block__container-textDate">Date</p>
+      </div>
+  </div>
+  </section> */
+}
+
 // блок
 // .header
 // .todo-list
