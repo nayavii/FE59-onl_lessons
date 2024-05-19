e501e35b23b737fa53c1eb929d70dbc8dda36b2a
Karasevgen1205 <karasevgen1205@yandex.ru>
2024-05-19 21:56:20 +0300

    add hw 2

--- hw_review.js
+++ hw_review.js
@@ -24,3 +24,45 @@ loadedTodos(15)
   .then(() => loadedTodos(7))
   .then(() => loadedTodos(3))
   .catch((error) => console.log(error));
+
+////////////////////////////////////////////////////////////////////////
+
+// Функция для получения поста по ID
+function fetchPostById(id) {
+  return fetch(`https://jsonplaceholder.typicode.com/todos/${id}`).then(
+    (res) => {
+      if (!res.ok) {
+        throw new Error(`Не удалось загрузить пост с ID ${id}`);
+      }
+      return res.json();
+    }
+  );
+}
+
+// Функция для создания HTML разметки поста
+function createPostHTML(post) {
+  const postDiv = document.createElement("div");
+  postDiv.classList.add("post");
+  postDiv.innerHTML = `
+      <h3>ID: ${post.id}</h3>
+      <p>${post.title}</p>
+  `;
+  return postDiv;
+}
+
+// Функция для отображения постов с использованием цепочек промисов
+function displayPostsWithPromiseChaining(postIds) {
+  let promiseChain = Promise.resolve();
+
+  postIds.forEach((postId) => {
+    promiseChain = promiseChain
+      .then(() => fetchPostById(postId))
+      .then((post) => {
+        const postHTML = createPostHTML(post);
+        postsContainer.appendChild(postHTML);
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  });
+}
