2099c1dc34cb63754dd085d1e77558682a2e7c56
karas.e <karas.e@ifuture.by>
2024-05-20 11:08:23 +0300

    hw

--- hw_review.js
+++ hw_review.js
@@ -1,3 +1,14 @@
+// На занятии обсудить
+
+// try/catch
+
+// ["Begin on Road A","Right on Road B","Right on Road C","Left on Road D"]
+// ['Begin on Road D', 'Right on Road C', 'Left on Road B', 'Left on Road A']
+
+// ['Begin on Lua Pkwy', 'Right on Sixth Alley', 'Right on 1st Cr']
+// ['Begin on 1st Cr', 'Left on Sixth Alley', 'Left on Lua Pkwy']
+
+////////////////////////////////////////////////////////////////////////
 const ul = document.querySelector(".bottom_block");
 
 function loadedTodos(todoId) {
@@ -66,3 +77,75 @@ function displayPostsWithPromiseChaining(postIds) {
       });
   });
 }
+////////////////////////////////////////////////////////////////////////
+// Promise chaining:
+function fetchPost(postNumber) {
+  return new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+
+      const posts = [
+        "Post 1",
+        "Post 2",
+        "Post 3",
+        "Post 4",
+      ];
+
+
+      if (posts[postNumber - 1]) {
+        resolve(posts[postNumber - 1]); 
+      } else {
+        reject(`Error: Post ${postNumber} not found`); 
+      }
+    }, Math.random() * 2000); 
+  });
+}
+
+
+function load_posts_in_order() {
+  fetchPost(15)
+    .then(post => {
+      console.log(post);
+      return fetchPost(23);
+    })
+    .then(post => {
+      console.log(post);
+      return fetchPost(7);
+    })
+    .then(post => {
+      console.log(post);
+      return fetchPost(3);
+    })
+    .then(post => {
+      console.log(post);
+    })
+    .catch(error => {
+      console.error(error); 
+    });
+}
+
+
+load_posts_in_order();
+
+//Async / await:
+
+
+async function loadPosts() {
+  try {
+    const post15 = await fetchPost(15);
+    console.log(post15);
+
+    const post23 = await fetchPost(23);
+    console.log(post23);
+
+    const post7 = await fetchPost(7);
+    console.log(post7);
+
+    const post3 = await fetchPost(3);
+    console.log(post3);
+  } catch (error) {
+    console.error(error); 
+  }
+}
+
+loadPosts();
