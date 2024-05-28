de43905a9b9634f655bc545fb823ad3fa1264139
karas.e <karas.e@ifuture.by>
2024-05-28 13:52:59 +0300

    add one hw

--- hw-review.js
+++ hw-review.js
@@ -74,6 +74,31 @@
 //   const newArray = addIntoStart(array, { name: 'торт', position: 1, isFixedPostion: false });
 //   console.log(newArray);
 
+////////////////////////////////////////////////////////////////////////////////////////////////
+// const array = [
+//     {name: 'кукла', position: 1, isFixedPosition: false},
+//     {name: 'дрель', position: 2, isFixedPosition: true},
+//     {name: 'сметана', position: 3, isFixedPosition: false},
+//     {name: 'штаны', position: 4, isFixedPosition: true},
+//     {name: 'колесо', position: 5, isFixedPosition: false},
+// ];
+
+// function addIntoStart(array, newObj) {
+//     const fixedObj = array.find(item => item.isFixedPosition == true);
+//     const fixedPositionIndex = fixedObj.position - 1;
+
+//     array.unshift(newObj);
+//     array.splice(fixedPositionIndex, 2, fixedObj, array[fixedPositionIndex]);
+//     array.forEach(item => {
+//         item.position = array.indexOf(item) + 1;
+//     })
+// }
+
+
+// addIntoStart(array, {name: 'торт', position: 1, isFixedPosition: false});
+// console.log(array)
+
+////////////////////////////////////////////////////////////////////////////////////////////////
 // Одно из решений:
   const array = [
     { name: 'кукла', position: 1, isFixedPostion: false },
